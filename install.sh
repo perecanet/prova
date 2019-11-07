ef8ca6dfaa4992041f63e19aee9776dad96a19cc
PereCanet <naur@localhost.localdomain>
2019-11-07 21:44:15 +0100

    ldap ok

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install ldapserver
+
+rm -rf /etc/openldap/slapd.d/*
+rm -rf /var/lib/ldap/*
+cp /opt/docker/DB_CONFIG /var/lib/ldap/.
+slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/organitzacio_edt.org.ldif
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/usuaris_edt.org.ldif
+chown -R ldap.ldap /etc/openldap/slapd.d
+chown -R ldap.ldap /var/lib/ldap
+cp /opt/docker/ldap.conf /etc/openldap/.
+
